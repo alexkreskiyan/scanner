42bfa4d8a7fe466899b68fc86b24f092b7375475
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-03-19 18:07:45 +0300

    server: init db migrations

---
+++ server/src/Server.Host/Db/DbMigrator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using DbUp;
+
+namespace Server.Host.Db;
+
+public static class DbMigrator
+{
+    public static void Migrate(string connectionString, string schema)
+    {
+        var result = DeployChanges.To
+            .PostgresqlDatabase(connectionString)
+            .WithTransactionPerScript()
+            .WithVariablesDisabled()
+            .WithExecutionTimeout(TimeSpan.FromSeconds(30))
+            .JournalToPostgresqlTable(schema, "log_dbup_migrations")
+            .WithScriptsEmbeddedInAssembly(
+                Assembly.GetCallingAssembly(),
+                s => s.Contains("Migrations")
+            )
+            .LogToConsole()
+            .Build()
+            .PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            throw new ApplicationException(result.ErrorScript + ":" + result.Error);
+        }
+    }
+}
---
+++ server/src/Server.Host/Db/Migrations/2024-03-19_T1_init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE document_type_configurations
+(
+    id                uuid        NOT NULL PRIMARY KEY,
+    type              text        NOT NULL
+);
