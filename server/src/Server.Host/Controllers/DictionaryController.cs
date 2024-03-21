e983b39ce56fb7009d9e5a1558643773a3aa971e
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-03-21 16:12:53 +0300

    server: define DictionaryController

---
+++ server/src/Server.Host/Controllers/DictionaryController.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Views;
+
+namespace Server.Host.Controllers;
+
+[ApiController]
+[Route("api/1.0/[controller]")]
+public class DictionaryController : ControllerBase
+{
+    private readonly Settings _settings;
+
+    public DictionaryController(Settings settings)
+    {
+        _settings = settings;
+    }
+
+    [HttpGet("document-types")]
+    [ProducesResponseType(typeof(List<string>), StatusCodes.Status200OK)]
+    public List<string> DocumentTypes()
+    {
+        return _settings.DocumentTypes;
+    }
+
+    [HttpGet("document-fields")]
+    [ProducesResponseType(
+        typeof(Dictionary<string, DocumentFieldConfigurationResponse>),
+        StatusCodes.Status200OK
+    )]
+    public Dictionary<string, DocumentFieldConfigurationResponse> DocumentFields()
+    {
+        return _settings.DocumentFields.ToDictionary(
+            x => x.Key,
+            x => new DocumentFieldConfigurationResponse
+            {
+                Label = x.Value.Label,
+                DataType = x.Value.DataType
+            }
+        );
+    }
+
+    [HttpGet("document-pages")]
+    [ProducesResponseType(typeof(List<string>), StatusCodes.Status200OK)]
+    public List<string> DocumentPages()
+    {
+        return _settings.DocumentPages;
+    }
+}
---
+++ server/src/Server.Views/DocumentFieldConfigurationResponse.cs
@@ -0,0 +1,7 @@
+namespace Server.Views;
+
+public class DocumentFieldConfigurationResponse
+{
+    public string Label { get; init; } = string.Empty;
+    public string DataType { get; init; } = string.Empty;
+}
